19fde9152c5a1c4f3941055d8161d84e84469d07
mohzaf66 <zafranraja5@gmail.com>
2024-06-16 00:13:48 +0100

    a

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,7 +1,10 @@
+import Link from "next/link"
 const NotFound = () => {
   return (
     <div>
-        <h2>Page Not Found</h2>
+        <h2>Not Found</h2>
+        <p>Sorry, the page you are looking for does not exist.</p>
+        <Link href="/">Return Home</Link>
     </div>
   )
 }
