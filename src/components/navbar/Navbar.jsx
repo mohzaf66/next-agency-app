67641e391a4d664035532fc81d9c864be43837c9
mohzaf66 <zafranraja5@gmail.com>
2024-06-15 23:17:12 +0100

    aa

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,15 @@
+import Links from "./links/Links"
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div>
+   
+    <div>Logo</div>
+    <div>
+      <Links/>
+    </div>
+
+    </div>
   )
 }
 
